a0efe6f482ed9550cdfcc7f14a890ddd5bb403b1
Thanga Ganapathy <ganapathy888@gmail.com>
2023-04-18 01:36:42 +0530

    Fix UMD build name

--- packages/vue-input-mask/vite.config.js
+++ packages/vue-input-mask/vite.config.js
@@ -1,6 +1,6 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,20 +8,20 @@ export default defineConfig({
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
-      entry: resolve(__dirname, "src/index.js"),
-      name: "vue-input-mask",
+      entry: resolve(__dirname, 'src/index.js'),
+      name: 'VueInputMask',
       // the proper extensions will be added
-      fileName: "index",
+      fileName: 'index',
     },
     rollupOptions: {
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ["vue"],
+      external: ['vue'],
       output: {
         // Provide global variables to use in the UMD build
         // for externalized deps
         globals: {
-          vue: "Vue",
+          vue: 'Vue',
         },
       },
     },
